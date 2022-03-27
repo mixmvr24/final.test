0f008b781330a5d6bfe5b918a08dbd73095d48d2
Igor Pozniak <mixmvr24@yandex.ru>
2022-03-27 03:39:38 +0300

    Clear project

--- resources/views/auth/private.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.shop')
-@section('content')
-    <div class="form-group text-center">
-        <div>
-            <span>Пользователь успешно авторизирован!</span>
-            <a href="{{route('main_page')}}">Для продолжения можете перейти на главную страницу!</a>
-        </div>
-    </div>
-@endsection
